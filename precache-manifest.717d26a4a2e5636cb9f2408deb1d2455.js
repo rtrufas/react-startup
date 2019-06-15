95300b748893cefd1d83f4b507614eba74af794f
rtrufas <rtrufas@gmail.com>
2019-06-15 17:15:08 +0300

    Updates

---
+++ precache-manifest.717d26a4a2e5636cb9f2408deb1d2455.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d09919ccd095179372027b69b2b13f51",
+    "url": "/acasa/index.html"
+  },
+  {
+    "revision": "1b1fd496bc8369f7aa1f",
+    "url": "/acasa/static/css/main.70898b30.chunk.css"
+  },
+  {
+    "revision": "d2bb60a0ba92ba5a23a3",
+    "url": "/acasa/static/js/2.9b7aa11c.chunk.js"
+  },
+  {
+    "revision": "1b1fd496bc8369f7aa1f",
+    "url": "/acasa/static/js/main.056758c3.chunk.js"
+  },
+  {
+    "revision": "491b1c9fbd2f6c77defd",
+    "url": "/acasa/static/js/runtime~main.89e73c8e.js"
+  },
+  {
+    "revision": "e03c595bd6c085ce5c96a2a1fa4db067",
+    "url": "/acasa/static/media/admitere.e03c595b.jpg"
+  },
+  {
+    "revision": "5118b508f8a6aa1ca3a88fc82146a571",
+    "url": "/acasa/static/media/bgbody.5118b508.jpg"
+  },
+  {
+    "revision": "1c861c1d3595c82d7264680ab3a6dd87",
+    "url": "/acasa/static/media/frame.1c861c1d.png"
+  }
+]);
