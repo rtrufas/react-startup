f8c9d66158def963d89bd14b314a7488c91db0fb
rtrufas <rtrufas@gmail.com>
2019-06-15 16:20:46 +0300

    fix warnings

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -15,8 +15,8 @@ export default class Home extends React.Component {
           pregatire profesionala si cu o mare capacitate de integrare in
           societate.
         </h1>
-        
-        <div classname="home-page__imagini-container">
+
+        <div className="home-page__imagini-container">
           <img src={cladire} alt="cladire" className="cladire" />
           <img src={cladire} alt="cladire" className="cladire" />
           <img src={cladire} alt="cladire" className="cladire" />
@@ -25,12 +25,12 @@ export default class Home extends React.Component {
         <div className="home-page__text-container">
           <div className="left">
             <p>
-              <b>Fundatia "Ioan Slavici"</b> din Timisoara a luat fiinta din dorinta
-              unui grup de personalitati din invatamantul superior timisorean de
-              a crea o institutie in sistem privat, care sa aseze mai presus de
-              orice formarea intelectuala a tinerilor cu o larga deschidere
-              culturala, buna pregatire profesionala si cu o mare capacitate de
-              integrare in societate.
+              <b>Fundatia "Ioan Slavici"</b> din Timisoara a luat fiinta din
+              dorinta unui grup de personalitati din invatamantul superior
+              timisorean de a crea o institutie in sistem privat, care sa aseze
+              mai presus de orice formarea intelectuala a tinerilor cu o larga
+              deschidere culturala, buna pregatire profesionala si cu o mare
+              capacitate de integrare in societate.
             </p>
             <p>
               Inca de la Infiintare, Fundatia pentru Cultura si Invatamant Ioan
@@ -93,72 +93,96 @@ export default class Home extends React.Component {
           </div>
 
           <div className="right">
-            <p><b>Liceul Ioan Slavici - pregatire pe Filiera
-            Tehnologica, Specializarea Tehnician in Activitati Economice, Profil
-            Servicii</b></p>. Fundaţia pentru Cultură şi Învăţământ “Ioan Slavici” din
+            <p>
+              <b>
+                Liceul Ioan Slavici - pregatire pe Filiera Tehnologica,
+                Specializarea Tehnician in Activitati Economice, Profil Servicii
+              </b>
+            </p>
+            . Fundaţia pentru Cultură şi Învăţământ “Ioan Slavici” din
             Timişoara, preocupată permanent de pregătirea profesională a
             tinerilor, în concordanţă cu cererile existente pe piaţa forţei de
             muncă, din dorinţa de a realiza un învăţământ cât mai util şi
             eficient condiţiilor actuale şi de perspectivă a României – în
             general, precum şi a Timişoarei – în special, prin Ordinul
             nr.24/23.04.2001 a hotărât înfinţarea Liceului “IOAN SLAVICI” în
-            Timişoara, coordonat de Fundaţia pentru Cultura si Invatamant "Ioan Slavici".
-            Oportunitatea înfinţării liceului rezidă şi din dorinţa creării unui
-            cadru de învăţământ integrat, activitatea liceală alăturându-se în
-            mod armonios activitatii universitare, a cărei activitate are
-            deja experienţa primului an.<p>Având în vedere orientarea de la
-            început a Fundaţiei către domeniul economic, (orientatare
-            manifestată prin înfinţarea în cadrul Universităţii “IOAN SLAVICI” a
-            Facultăţii de Ştiinţe Economice, cu specialitatea autorizată
-            Contabilitate şi informatică de gestiune), s-a solicitat aprobarea
-            înfinţării liceului "Ioan Slavici", planul de şcolarizare pentru
-            anul şcolar 2001/2002, cuprinzând, pentru început, 2 clase a IX-a cu
-            50 de elevi, Filiera TEHNOLOGICĂ, Profil SERVICII,
-            Specializarea Tehnician in Activitati Economice.</p>  Liceul şi-a început activitatea cu 2 clase a
-            IX-a, înregistrându-se în Timişoara ca liceu particular, alături de
-            alte licee particulare: Liceul “Milenium” şi Liceul “Mihai
-            Eminescu”, respectiv Grupul Şcolar de arte şi meserii „Spiru Haret”.
-            Baza materială de care dispune Liceul “IOAN SLAVICI” în prezent,
-            poate susţine în condiţii bune pregătirea elevilor în domeniul
-            economic. Totodată, prospectând cererile populaţiei de elevi, există
-            un mare interes pentru pregătirea şcolară în domeniul economic, fapt
-            ce vine în întâmpinarea şi cererilor de pe piaţa muncii, cu
-            posibilităţi evidente de continuare a studiilor în cadrul
-            Universităţii “IOAN SLAVICI”, parte integrantă a Fundaţiei. <li>
-            Participare în masă la ACTIVITǍŢI EXTRACURRICULARE ce au avut
-            menirea să îmbogăţească cunoştinţele şi să formeze deprinderi, să
-            stimuleze şi să amelioreze comportamente sociale (concursuri,
-            expoziţii cu lucrări ale elevilor, vizite, excursii, acţiuni de
-            voluntariat – colectare ajutoare pentru copii şi bătrâni, colectare
-            de materiale refolosibile şi valorificarea lor, serbări şcolare,
-            corespondenţa cu colegi din ţară şi străinătate, concursuri sportive
-            la nivelul şcolii , între clase paralele şi între şcoli etc.) -
-            obţinerea de diplome, cupe, premii la concursuri interşcolare
-            sportive.</li> <li>Participare la concursuri şi activităţi desfăşurate în
-            cadrul unor PROIECTE DE PARTENERIAT / COLABORARE la nivel local,
-            naţional şi internaţional care s-au soldat cu:<p>- obţinerea de
-            diplome şi participare la concursuri şi activităţi desfăşurate în
-            comun la nivel local şi interjudeţean;</p>  - mediatizare în presa locală
-            şi publicaţii de specialitate; <p>- întemeierea unor relaţii de
-            prietenie şi colaborare atât între elevi cât şi cadrele de la
-            şcolile implicate;</p> - corespondenţa prin poşta tradiţională cât şi
-            electronică.</li> <p>Activităţile specifice de îmbunătăţire a calităţii
-            învăţământului sunt cele realizate prin activităţi de pregătire
-            suplimentară la limba şi literatura română, matematică, geograe,
-            istorie, limbi moderne. Rezultat: îmbunătăţirea procentului de
-            promovare a BACALAUREATULUI. <p>-creşterea numărului de participanţi şi
-            rezultate foarte bune la concursurile pe discipline la care au
-            participat elevii.</p></p> In anul scolar 2018/2019, Liceul tehnologic “IOAN
-            SLAVICI” din Timisoara ofera o clasa a IX-a, ( 28 locuri ), filiera
-            TEHNOLOGICA, profilul SERVICII, specializarea TEHNICIAN IN ACTIVITATI
-            ECONOMICE – 4ani.<p>Se accepta, prin transfer si cu sustinerea
-            eventualelor examene de diferenta, elevi in clasele a X-a, a XI-a si
-            a XII-a.</p>  Liceul este acreditat din anul scolar 2009 pe baza
-            ordinului privind acordarea acreditarii nr.4126/11.05.2009 eliberat
-            de Ministerul Educatiei, Cercetarii si Inovarii – Cabinetul
-            Ministrului; Inscrierile se fac in perioada de iunie-septembrie
-            2018, la sediul Liceului. (Timisoara, Str. Paunescu Podeanu nr. 144,
-            Tel. 0256-213108)
+            Timişoara, coordonat de Fundaţia pentru Cultura si Invatamant "Ioan
+            Slavici". Oportunitatea înfinţării liceului rezidă şi din dorinţa
+            creării unui cadru de învăţământ integrat, activitatea liceală
+            alăturându-se în mod armonios activitatii universitare, a cărei
+            activitate are deja experienţa primului an.
+            <p>
+              Având în vedere orientarea de la început a Fundaţiei către
+              domeniul economic, (orientatare manifestată prin înfinţarea în
+              cadrul Universităţii “IOAN SLAVICI” a Facultăţii de Ştiinţe
+              Economice, cu specialitatea autorizată Contabilitate şi
+              informatică de gestiune), s-a solicitat aprobarea înfinţării
+              liceului "Ioan Slavici", planul de şcolarizare pentru anul şcolar
+              2001/2002, cuprinzând, pentru început, 2 clase a IX-a cu 50 de
+              elevi, Filiera TEHNOLOGICĂ, Profil SERVICII, Specializarea
+              Tehnician in Activitati Economice.
+            </p>{" "}
+            Liceul şi-a început activitatea cu 2 clase a IX-a, înregistrându-se
+            în Timişoara ca liceu particular, alături de alte licee particulare:
+            Liceul “Milenium” şi Liceul “Mihai Eminescu”, respectiv Grupul
+            Şcolar de arte şi meserii „Spiru Haret”. Baza materială de care
+            dispune Liceul “IOAN SLAVICI” în prezent, poate susţine în condiţii
+            bune pregătirea elevilor în domeniul economic. Totodată, prospectând
+            cererile populaţiei de elevi, există un mare interes pentru
+            pregătirea şcolară în domeniul economic, fapt ce vine în
+            întâmpinarea şi cererilor de pe piaţa muncii, cu posibilităţi
+            evidente de continuare a studiilor în cadrul Universităţii “IOAN
+            SLAVICI”, parte integrantă a Fundaţiei.{" "}
+            <li>
+              Participare în masă la ACTIVITǍŢI EXTRACURRICULARE ce au avut
+              menirea să îmbogăţească cunoştinţele şi să formeze deprinderi, să
+              stimuleze şi să amelioreze comportamente sociale (concursuri,
+              expoziţii cu lucrări ale elevilor, vizite, excursii, acţiuni de
+              voluntariat – colectare ajutoare pentru copii şi bătrâni,
+              colectare de materiale refolosibile şi valorificarea lor, serbări
+              şcolare, corespondenţa cu colegi din ţară şi străinătate,
+              concursuri sportive la nivelul şcolii , între clase paralele şi
+              între şcoli etc.) - obţinerea de diplome, cupe, premii la
+              concursuri interşcolare sportive.
+            </li>{" "}
+            <li>
+              Participare la concursuri şi activităţi desfăşurate în cadrul unor
+              PROIECTE DE PARTENERIAT / COLABORARE la nivel local, naţional şi
+              internaţional care s-au soldat cu:
+              <p>
+                - obţinerea de diplome şi participare la concursuri şi
+                activităţi desfăşurate în comun la nivel local şi interjudeţean;
+              </p>{" "}
+              - mediatizare în presa locală şi publicaţii de specialitate;{" "}
+              <p>
+                - întemeierea unor relaţii de prietenie şi colaborare atât între
+                elevi cât şi cadrele de la şcolile implicate;
+              </p>{" "}
+              - corespondenţa prin poşta tradiţională cât şi electronică.
+            </li>{" "}
+            <p>
+              Activităţile specifice de îmbunătăţire a calităţii învăţământului
+              sunt cele realizate prin activităţi de pregătire suplimentară la
+              limba şi literatura română, matematică, geograe, istorie, limbi
+              moderne. Rezultat: îmbunătăţirea procentului de promovare a
+              BACALAUREATULUI. -creşterea numărului de participanţi şi rezultate
+              foarte bune la concursurile pe discipline la care au participat
+              elevii.
+            </p>{" "}
+            In anul scolar 2018/2019, Liceul tehnologic “IOAN SLAVICI” din
+            Timisoara ofera o clasa a IX-a, ( 28 locuri ), filiera TEHNOLOGICA,
+            profilul SERVICII, specializarea TEHNICIAN IN ACTIVITATI ECONOMICE –
+            4ani.
+            <p>
+              Se accepta, prin transfer si cu sustinerea eventualelor examene de
+              diferenta, elevi in clasele a X-a, a XI-a si a XII-a.
+            </p>{" "}
+            Liceul este acreditat din anul scolar 2009 pe baza ordinului privind
+            acordarea acreditarii nr.4126/11.05.2009 eliberat de Ministerul
+            Educatiei, Cercetarii si Inovarii – Cabinetul Ministrului;
+            Inscrierile se fac in perioada de iunie-septembrie 2018, la sediul
+            Liceului. (Timisoara, Str. Paunescu Podeanu nr. 144, Tel.
+            0256-213108)
           </div>
         </div>
       </div>
